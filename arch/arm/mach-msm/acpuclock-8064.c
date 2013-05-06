ce827e0fe1569afcc4c6040a49c876fed054bac7
Bryan Smith <zarboz@gmail.com>
2013-05-06 01:32:40 -0600

    arch/arm acpuclock-8064.c Fix: typo during cmdline coding
    Signed-off-by: Bryan Smith <zarboz@gmail.com>

--- arch/arm/mach-msm/acpuclock-8064.c
+++ arch/arm/mach-msm/acpuclock-8064.c
@@ -661,7 +661,7 @@ uint32_t __init acpu_check_khz_value(unsigned long khz)
 {
 	struct acpu_level *f;
 
-	if (khz > 2106000)
+	if (khz > 2160000)
 		return CONFIG_MSM_CPU_FREQ_MAX;
 
 	if (khz < 192000)
