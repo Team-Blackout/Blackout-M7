09ad1cd619fdb040a4ac789b821b6084a50d4e94
Bryan Smith <zarboz@gmail.com>
2013-05-08 19:05:02 -0600

    input/ synaptics_3200 Add: hooks for BB mod by tbalden
    Signed-off-by: Bryan Smith <zarboz@gmail.com>

--- include/linux/synaptics_i2c_rmi.h
+++ include/linux/synaptics_i2c_rmi.h
@@ -168,7 +168,7 @@ enum {
 	INTR_SOURCE,
 	FUNCTION
 };
-
+extern uint8_t touchscreen_is_on(void); 
 #ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_SWEEP2WAKE
 /* Sweep2Wake */
 extern void sweep2wake_setdev(struct input_dev * input_device);
