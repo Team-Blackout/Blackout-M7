75cc9d4a0ef1922e5f5501f895253ac7becb5b33
Bryan Smith <zarboz@gmail.com>
2013-05-12 19:10:42 -0600

    arch/arm/mach-msm/ board-m7-gpu this got lost in cherry-picks
    Signed-off-by: Bryan Smith <zarboz@gmail.com>

--- arch/arm/mach-msm/board-m7-gpu.c
+++ arch/arm/mach-msm/board-m7-gpu.c
@@ -436,6 +436,11 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
 			.gpu_freq = 450000000,
+			.bus_freq = 5,
+			.io_fraction = 0,
+		},
+        {
+			.gpu_freq = 400000000,
 			.bus_freq = 4,
 			.io_fraction = 0,
 		},
@@ -455,7 +460,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 2,
-	.num_levels = 4,
+	.num_levels = 5,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/10,
 	.nap_allowed = true,
