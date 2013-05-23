4a2dd3dd209857cc161a44caec4989ee91545ee6
Bryan Smith <zarboz@gmail.com>
2013-05-22 22:38:38 -0600

    arch/arm/mach-msm/acpuclock Refining VDD tables
    Signed-off-by: Bryan Smith <zarboz@gmail.com>

--- arch/arm/mach-msm/acpuclock-8064.c
+++ arch/arm/mach-msm/acpuclock-8064.c
@@ -51,7 +51,7 @@ static struct scalable scalable[] __initdata = {
 		.aux_clk_sel = 3,
 		.sec_clk_sel = 2,
 		.l2cpmr_iaddr = 0x4501,
-		.vreg[VREG_CORE] = { "krait0", 1350000 },
+		.vreg[VREG_CORE] = { "krait0", 1300000 },
 		.vreg[VREG_MEM]  = { "krait0_mem", 1250000 },
 		.vreg[VREG_DIG]  = { "krait0_dig", 1250000 },
 		.vreg[VREG_HFPLL_A] = { "krait0_hfpll", 1800000 },
@@ -62,7 +62,7 @@ static struct scalable scalable[] __initdata = {
 		.aux_clk_sel = 3,
 		.sec_clk_sel = 2,
 		.l2cpmr_iaddr = 0x5501,
-		.vreg[VREG_CORE] = { "krait1", 1350000 },
+		.vreg[VREG_CORE] = { "krait1", 1300000 },
 		.vreg[VREG_MEM]  = { "krait1_mem", 1250000 },
 		.vreg[VREG_DIG]  = { "krait1_dig", 1250000 },
 		.vreg[VREG_HFPLL_A] = { "krait1_hfpll", 1800000 },
@@ -73,7 +73,7 @@ static struct scalable scalable[] __initdata = {
 		.aux_clk_sel = 3,
 		.sec_clk_sel = 2,
 		.l2cpmr_iaddr = 0x6501,
-		.vreg[VREG_CORE] = { "krait2", 1350000 },
+		.vreg[VREG_CORE] = { "krait2", 1300000 },
 		.vreg[VREG_MEM]  = { "krait2_mem", 1250000 },
 		.vreg[VREG_DIG]  = { "krait2_dig", 1250000 },
 		.vreg[VREG_HFPLL_A] = { "krait2_hfpll", 1800000 },
@@ -84,7 +84,7 @@ static struct scalable scalable[] __initdata = {
 		.aux_clk_sel = 3,
 		.sec_clk_sel = 2,
 		.l2cpmr_iaddr = 0x7501,
-		.vreg[VREG_CORE] = { "krait3", 1350000 },
+		.vreg[VREG_CORE] = { "krait3", 1300000 },
 		.vreg[VREG_MEM]  = { "krait3_mem", 1250000 },
 		.vreg[VREG_DIG]  = { "krait3_dig", 1250000 },
 		.vreg[VREG_HFPLL_A] = { "krait3_hfpll", 1800000 },
@@ -671,11 +671,11 @@ uint32_t __init acpu_check_khz_value(unsigned long khz)
 	if (khz > 2160000)
 		return CONFIG_MSM_CPU_FREQ_MAX;
 
-	if (khz < 192000)
+	if (khz < 162000)
 		return CONFIG_MSM_CPU_FREQ_MIN;
 
 	for (f = tbl_PVS0_1700MHz,tbl_PVS1_1700MHz,tbl_PVS2_1700MHz,tbl_PVS3_1700MHz,tbl_PVS4_1700MHz,tbl_PVS5_1700MHz,tbl_PVS6_1700MHz; f->speed.khz != 0; f++) {
-		if (khz < 192000) {
+		if (khz < 162000) {
 			if (f->speed.khz == (khz*1000))
 				return f->speed.khz;
 			if ((khz*1000) > f->speed.khz) {
