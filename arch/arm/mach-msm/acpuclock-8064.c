fecf160989a29e48045937bda0a800450246437e
Bryan Smith <zarboz@gmail.com>
2013-05-23 01:27:54 -0600

    arch/arm/mach-msm/acpuclock Fixing VDD to more stable values for boot
    Signed-off-by: Bryan Smith <zarboz@gmail.com>

--- arch/arm/mach-msm/acpuclock-8064.c
+++ arch/arm/mach-msm/acpuclock-8064.c
@@ -340,7 +340,7 @@ static struct acpu_level tbl_PVS2_1700MHz[] __initdata = {
 };
 
 static struct acpu_level tbl_PVS3_1700MHz[] __initdata = {
-    { 1, {   162000, PLL_8, 0, 0x00 }, L2(0),   925000 },	
+    { 1, {   162000, PLL_8, 0, 0x00 }, L2(0),   900000 },	
 	{ 1, {   192000, PLL_8, 0, 0x00 }, L2(0),   900000 },
 	{ 1, {	 216000, PLL_8, 0, 0x00 }, L2(0),   900000 },
 	{ 1, { 	 270000, PLL_8, 0, 0x00 }, L2(0),   900000 },
@@ -368,7 +368,7 @@ static struct acpu_level tbl_PVS3_1700MHz[] __initdata = {
 };
 
 static struct acpu_level tbl_PVS4_1700MHz[] __initdata = {
-	{ 1, {   162000, PLL_8, 0, 0x00 }, L2(0),   925000 },	
+	{ 1, {   162000, PLL_8, 0, 0x00 }, L2(0),   900000 },	
 	{ 1, {   192000, PLL_8, 0, 0x00 }, L2(0),   900000 },
 	{ 1, {	 216000, PLL_8, 0, 0x00 }, L2(0),   900000 },
 	{ 1, { 	 270000, PLL_8, 0, 0x00 }, L2(0),   900000 },
@@ -396,7 +396,7 @@ static struct acpu_level tbl_PVS4_1700MHz[] __initdata = {
 };
 
 static struct acpu_level tbl_PVS5_1700MHz[] __initdata = {
-	{ 1, {   162000, PLL_8, 0, 0x00 }, L2(0),   900000 },	
+	{ 1, {   162000, PLL_8, 0, 0x00 }, L2(0),   875000 },	
 	{ 1, {   192000, PLL_8, 0, 0x00 }, L2(0),   875000 },
 	{ 1, {	 216000, PLL_8, 0, 0x00 }, L2(0),   875000 },
 	{ 1, { 	 270000, PLL_8, 0, 0x00 }, L2(0),   875000 },
@@ -424,7 +424,7 @@ static struct acpu_level tbl_PVS5_1700MHz[] __initdata = {
 };
 
 static struct acpu_level tbl_PVS6_1700MHz[] __initdata = {
-	{ 1, {   162000, PLL_8, 0, 0x00 }, L2(0),   900000 },	
+	{ 1, {   162000, PLL_8, 0, 0x00 }, L2(0),   875000 },	
 	{ 1, {   192000, PLL_8, 0, 0x00 }, L2(0),   875000 },
 	{ 1, {	 216000, PLL_8, 0, 0x00 }, L2(0),   875000 },
 	{ 1, { 	 270000, PLL_8, 0, 0x00 }, L2(0),   875000 },
