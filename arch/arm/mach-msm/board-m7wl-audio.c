e167054469420916919f87abca4d13c3075c7c7f
Bryan Smith <zarboz@gmail.com>
2013-08-07 21:10:10 -0400

    Update of audio sources from 4.2.2 gpe source
    Signed-off-by: Lens-F <tnoah12@gmail.com>
    
     Author:    Bryan Smith <zarboz@gmail.com>

--- arch/arm/mach-msm/board-m7wl-audio.c
+++ arch/arm/mach-msm/board-m7wl-audio.c
@@ -41,27 +41,41 @@ static int m7wl_get_hw_component(void)
 
 static int m7wl_enable_digital_mic(void)
 {
-    printk(KERN_INFO "[XD Only] m7_enable_digital_mic:skuid=0x%x, system_rev=%x\n", skuid, system_rev);
-    
-    if ((system_rev == XA)||(system_rev == XB)||(system_rev == XC)){
-        return 0;
-    }
-    else if (system_rev == XD){
-        if ((skuid & 0xFF) == 0x3) {
-            printk(KERN_INFO "(skuid & 0xFF) == 0x3\n");
-            return 1;
-        }
-        else if ((skuid & 0xFF) == 0x4) {
-            printk(KERN_INFO "(skuid & 0xFF) == 0x4\n");
-            return 1;
-        }
-        else{
-            return 0;
-        }
-    }
-    else{
-        return 1;
-    }
+	int ret;
+	if((system_rev == XA)||(system_rev == XB)){
+		ret = 0;
+	}
+	else if ((system_rev == XC)||(system_rev == XD)){
+		if (((skuid & 0xFF) == 0x0B) ||
+			((skuid & 0xFF) == 0x0D) ||
+			((skuid & 0xFF) == 0x0C) ||
+			((skuid & 0xFF) == 0x0E) ||
+			((skuid & 0xFF) == 0x0F) ||
+			((skuid & 0xFF) == 0x10) ||
+			((skuid & 0xFF) == 0x11) ||
+			((skuid & 0xFF) == 0x12) ||
+			((skuid & 0xFF) == 0x13) ||
+			((skuid & 0xFF) == 0x14) ||
+			((skuid & 0xFF) == 0x15)) {
+			ret = 1;
+		}
+		else{
+			ret = 0;
+		}
+	}
+	else{
+		if ((skuid & 0xFFF00) == 0x34C00) {
+			ret = 1;
+		}
+		else if ((skuid & 0xFFF00) == 0x38900) {
+			ret = 2;
+		}
+		else {
+			ret = 3;
+		}
+	}
+	printk(KERN_INFO "m7wl_enable_digital_mic:skuid=0x%x, system_rev=%x return %d\n", skuid, system_rev, ret);
+	return ret;
 }
 
 void apq8064_set_q6_effect_mode(int mode)
@@ -79,7 +93,7 @@ int apq8064_get_q6_effect_mode(void)
 
 int apq8064_get_24b_audio(void)
 {
-       return 1;
+	return 1;
 }
 
 static struct acoustic_ops acoustic = {
@@ -97,7 +111,7 @@ static struct msm_pcm_routing_ops rops = {
 };
 
 static struct msm_compr_q6_ops cops = {
-       .get_24b_audio = apq8064_get_24b_audio,
+	.get_24b_audio = apq8064_get_24b_audio,
 };
 
 
